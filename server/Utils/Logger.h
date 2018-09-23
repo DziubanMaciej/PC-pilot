56366c3c9ce31362bae6111ad8250ff456065f9f
DziubanMaciejB <dziuban.maciej@gmail.com>
2018-09-23 17:56:41 +0200

    [server] Add Logger function for containers

--- server/Utils/Logger.h
+++ server/Utils/Logger.h
@@ -11,22 +11,37 @@ class Logger {
 
 	template <typename T, typename... Args>
 	void _log(const T& arg, const Args&... args) {
-		this->out << arg;
+		dump(arg, false);
 		_log(args...);
 	}
 
 	template <typename... Args>
 	void _log() {
-		this->out << '\n';
+		dump("", true);
 	}
 
 public:
 	static Logger log;
 
 	template <typename... Args>
-	Logger& operator()(const Args&... args) {
+	void operator()(const Args&... args) {
 		std::unique_lock<std::mutex> lock{ this->mutex };
 		_log(args...);
-		return *this;
+	}
+
+	template <typename Iterator>
+	void dumpContainer(Iterator begin, Iterator end, bool insertNewLines = true) {
+		std::unique_lock<std::mutex> lock{ this->mutex };
+		for (auto it = begin; it < end; it++) {
+			dump(*it, insertNewLines);
+		}
+	}
+
+	template <typename T>
+	void dump(const T &arg, bool insertNewLines = true) {
+		this->out << arg;
+		if (insertNewLines) {
+			this->out << "\n";
+		}
 	}
 };
